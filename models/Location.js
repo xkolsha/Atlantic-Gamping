f6f2763db48a6bf6d160ec1613ce7e50ca34eae7
xkolsha <aviad85@outlook.com>
2023-08-08 20:02:04 -0300

    updated datat to correct order

--- models/Location.js
+++ models/Location.js
@@ -1,63 +1,62 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
 
-class Location extends Model {}
+class location extends Model {}
 
-Location.init(
-    {
-        id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            primaryKey: true,
-            autoIncrement: true,
-        },
-        city: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        province: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        page_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        img_url: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        description: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        short_desc: {
-            type: DataTypes.TEXT,
-            allowNull: false,
-        },
-        price: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        category_id: {
-            type: DataTypes.INTEGER,
-            references: {
-                model: 'category',
-                key: 'id',
-            },
-        },
+location.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    {
-        sequelize,
-        freezeTableName: true,
-        underscored: true,
-        modelName: 'Location',
-    }
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    province: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    page_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    img_url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    short_desc: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "location",
+  }
 );
 
-module.exports = Location;
-
+module.exports = location;
--- models/index.js
+++ models/index.js
@@ -1,19 +1,19 @@
-const User = require('./User');
-const Creator = require('./Creator');
-const Category = require('./Category');
-const Location = require('./Location');
-const Features = require('./Features');
+const User = require("./User");
+const Creator = require("./Creator");
+const Category = require("./Category");
+const Location = require("./location");
+const Features = require("./Features");
 
 Category.hasMany(Location, {
-    foreignKey: 'category_id',
+  foreignKey: "category_id",
 });
 
 Location.belongsTo(Category, {
-  foreignKey: 'category_id',
+  foreignKey: "category_id",
 });
 
 Features.hasMany(Location, {
-  foreignKey: 'location_id',
+  foreignKey: "location_id",
 });
 
-module.exports = { User, Creator, Category, Location, Features };
+module.exports = { User, Creator, Category, Features, Location };
