61efde42ed789cbcbc74b3967c3385db1f260024
xkolsha <aviad85@outlook.com>
2023-08-08 20:45:54 -0300

    test

--- models/index.js
+++ models/index.js
@@ -1,11 +1,7 @@
 const User = require("./User");
 const Creator = require("./Creator");
 const Category = require("./Category");
-<<<<<<< HEAD
-const Location = require("./location");
-=======
 const Location = require("./Location");
->>>>>>> 2e9a6d846022e729681834436fb6cc41089478a6
 const Features = require("./Features");
 
 Category.hasMany(Location, {
@@ -20,4 +16,4 @@ Features.hasMany(Location, {
   foreignKey: "location_id",
 });
 
-module.exports = { User, Creator, Category, Features, Location };
+module.exports = { User, Creator, Category, Location, Features };
