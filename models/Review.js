5fa507e6c646ec629accb434bb2a8c2e7f7f7bb2
xkolsha <aviad85@outlook.com>
2023-08-10 20:10:51 -0300

    added review model

---
+++ models/Review.js
@@ -0,0 +1,39 @@
+// models/Review.js
+
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Review extends Model {}
+
+Review.init(
+  {
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: true,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        min: 1,
+        max: 5,
+      },
+    },
+    location_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "listing",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: true,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "review",
+  }
+);
+
+module.exports = Review;
