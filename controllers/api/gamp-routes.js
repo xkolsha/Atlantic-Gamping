3920da482ade5f2b3d5ff3a691e663d8dae594cb
terenceDev <terence.arseneault@hotmail.com>
2023-08-11 22:04:43 -0300

    added logic for gamp

--- controllers/api/gamp-routes.js
+++ controllers/api/gamp-routes.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 
 router.get("/", async (req, res) => {
   res.render("index", { pageTitle: "Home" });
+  console.log("Gamp");
 });
 
 module.exports = router;
