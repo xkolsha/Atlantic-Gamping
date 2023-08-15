cc4f2b24392b98a37f0ad604553db4c3f27c92c5
terenceDev <terence.arseneault@hotmail.com>
2023-08-15 18:24:01 -0300

    bug fix

--- public/js/gamp.js
+++ public/js/gamp.js
@@ -204,8 +204,10 @@ let question3 = () => {
 
 // function to query location
 const runQuery = async () => {
-  const response = await fetch("http://localhost:3001/api/gamp");
-  // const response = await fetch("https://atlantic-glamping-7ef96dd8177e.herokuapp.com/api/gamp");
+  // const response = await fetch("http://localhost:3001/api/gamp");
+  const response = await fetch(
+    "https://atlantic-glamping-7ef96dd8177e.herokuapp.com/api/gamp"
+  );
 
   const data = await response.json();
 
